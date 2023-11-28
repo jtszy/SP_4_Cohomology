4b01436469a3e34311022a50252526a3a9735780
jtszy <jt.szymanski@student.uw.edu.pl>
2023-11-28 12:18:10 +0100

    renaming for symplectic groups

--- scripts/induction_experiments.jl
+++ scripts/induction_experiments.jl
@@ -6,58 +6,60 @@ LinearAlgebra.BLAS.set_num_threads(Sys.CPU_THREADS÷2)
 
 using LowCohomologySOS
 using SP_4_Cohomology
-using StarAlgebras
+# using StarAlgebras
 using Groups
 
 
 # TODO: adjust the whole code below to sp2ns
 
-const N = 4
-const M = 5
+const N = 2
+const M = 3
 
-i = LowCohomologySOS.sln_slm_embedding(N, M)
+i_ = SP_4_Cohomology.sln_slm_embedding(3,3)
 
-slN = i.source
-slM = i.target
+i = SP_4_Cohomology.sp2n_sp2m_embedding(2*N, 2*M)
+
+Sp2N = i.source
+Sp2M = i.target
 
 const half_radius = 2
 
-slN_S = gens(slN);
-slN_S_inv = let s = slN_S
+Sp2N_S = gens(Sp2N);
+Sp2N_S_inv = let s = Sp2N_S
     [s; inv.(s)]
 end;
-slM_S = gens(slM);
-slM_S_inv = let s = slM_S
+Sp2M_S = gens(Sp2M);
+Sp2M_S_inv = let s = Sp2M_S
     [s; inv.(s)]
 end;
 
-slN_half_basis, slN_sizes = Groups.wlmetric_ball(slN_S_inv, radius = half_radius);
-slM_half_basis, slM_sizes = Groups.wlmetric_ball(slM_S_inv, radius = half_radius);
+Sp2N_half_basis, Sp2N_sizes = Groups.wlmetric_ball(Sp2N_S_inv, radius = half_radius);
+Sp2M_half_basis, Sp2M_sizes = Groups.wlmetric_ball(Sp2M_S_inv, radius = half_radius);
 
-slN_Δ₁, slN_Iₙ, slN_Δ₁⁺, slN_Δ₁⁻ = LowCohomologySOS.laplacians(slN, slN_half_basis, slN_S, sq_adj_op_ = "adj");
-slM_Δ₁, slM_Iₙ, slM_Δ₁⁺, slM_Δ₁⁻ = LowCohomologySOS.laplacians(slM, slM_half_basis, slM_S, sq_adj_op_ = "adj");
-slN_sq, slN_adj, slN_op = LowCohomologySOS.sq_adj_op(slN_Δ₁⁻, slN_S)
-slM_sq, slM_adj, slM_op = LowCohomologySOS.sq_adj_op(slM_Δ₁⁻, slM_S)
+Sp2N_Δ₁, Sp2N_Iₙ, Sp2N_Δ₁⁺, Sp2N_Δ₁⁻ = LowCohomologySOS.laplacians(Sp2N, Sp2N_half_basis, Sp2N_S, sq_adj_op_ = "adj");
+Sp2M_Δ₁, Sp2M_Iₙ, Sp2M_Δ₁⁺, Sp2M_Δ₁⁻ = LowCohomologySOS.laplacians(Sp2M, Sp2M_half_basis, Sp2M_S, sq_adj_op_ = "adj");
+Sp2N_sq, Sp2N_adj, Sp2N_op = LowCohomologySOS.sq_adj_op(Sp2N_Δ₁⁻, Sp2N_S)
+Sp2M_sq, Sp2M_adj, Sp2M_op = LowCohomologySOS.sq_adj_op(Sp2M_Δ₁⁻, Sp2M_S)
 
-RG_prime = parent(first(slM_Δ₁⁺))
+RG_prime = parent(first(Sp2M_Δ₁⁺))
 
-Δ₁⁺_emb = LowCohomologySOS.embed_matrix(slN_Δ₁⁺, i, RG_prime)
-adj_emb = LowCohomologySOS.embed_matrix(slN_adj, i, RG_prime)
+Δ₁⁺_emb = LowCohomologySOS.embed_matrix(Sp2N_Δ₁⁺, i, RG_prime)
+adj_emb = LowCohomologySOS.embed_matrix(Sp2N_adj, i, RG_prime)
 
-@assert parent(first(Δ₁⁺_emb)) == parent(first(adj_emb)) == parent(first(slM_Δ₁⁺)) == parent(first(slM_adj))
+@assert parent(first(Δ₁⁺_emb)) == parent(first(adj_emb)) == parent(first(Sp2M_Δ₁⁺)) == parent(first(Sp2M_adj))
 
 using PermutationGroups
 
 Δ₁⁺_emb_symmetrized = let
     Σ = PermutationGroups.SymmetricGroup(3)
-    LowCohomologySOS.weyl_symmetrize_matrix(Δ₁⁺_emb, Σ, LowCohomologySOS._conj, slM_S)
+    LowCohomologySOS.weyl_symmetrize_matrix(Δ₁⁺_emb, Σ, LowCohomologySOS._conj, Sp2M_S)
 end
 
 adj_emb_symmetrized = let
     Σ = PermutationGroups.SymmetricGroup(3)
-    LowCohomologySOS.weyl_symmetrize_matrix(adj_emb, Σ, LowCohomologySOS._conj, slM_S)
+    LowCohomologySOS.weyl_symmetrize_matrix(adj_emb, Σ, LowCohomologySOS._conj, Sp2M_S)
 end
 
 # TODO change scalars below:
-24*slM_Δ₁⁺-Δ₁⁺_emb_symmetrized # it looks like symmetrization works for upper Laplacians!
-24*slM_adj-adj_emb_symmetrized # Adj symmetrizes as well with the same pace!!
+24*Sp2M_Δ₁⁺-Δ₁⁺_emb_symmetrized # it looks like symmetrization works for upper Laplacians!
+24*Sp2M_adj-adj_emb_symmetrized # Adj symmetrizes as well with the same pace!!
